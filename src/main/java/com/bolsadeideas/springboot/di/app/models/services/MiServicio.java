dbf40e7543901ee213c26f02a8d514aa0c9e42b0
Duber Alexander Galvis Giraldo <duber.alexander@gmail.com>
2023-06-27 19:07:22 -0500

    creación de proyecto para probar inyeccion de dependencias

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.di.app.models.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MiServicio {
+	
+	public String operacion() {
+		
+		return "operacion proceso...";
+	}
+}
